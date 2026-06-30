ed70aa45c10446304462e063fca82d86cf45b806
Irfan Karim <irfanstudent2008@gmail.com>
2026-06-30 19:29:47 +0500

    Add CommonJS module for multiplication function

---
+++ modules/modules/modules/modules/commonjs-example.js
@@ -0,0 +1,17 @@
+
+
+function multiply(a, b) {
+
+    return a * b;
+
+}
+
+module.exports = multiply;
+
+// Import Example
+
+/*
+const multiply = require("./multiply");
+
+console.log(multiply(4,5));
+*/
