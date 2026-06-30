cf7712d5062b68a8f6578a709e65c78fab494afa
Irfan Karim <irfanstudent2008@gmail.com>
2026-06-30 19:29:19 +0500

    Create app.js

---
+++ modules/modules/modules/app.js
@@ -0,0 +1,13 @@
+/*
+Importing ES Modules
+*/
+
+import greet from "./person.js";
+
+import { add, subtract } from "./math.js";
+
+greet("Ali");
+
+console.log(add(10, 5));
+
+console.log(subtract(10, 5));
